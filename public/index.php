c4b50cc60b81f4eeeb2a010d9a84ac781cc4650e
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-01 09:01:19 +1000

    Added HTML5 Kickstart

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
-<?php
+<!DOCTYPE html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+<head>
+    <script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
+    <script src="js/kickstart.js" type="text/javascript"></script>
+    <link href="css/kickstart.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    
+</body>
+    
+    
+</html>
