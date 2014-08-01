12a2ae21357110c57bc1ff0b6850f4e6c7e488d9
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-01 10:01:45 +1000

    REmoved

--- public/index.php
+++ public/index.php
@@ -3,8 +3,7 @@
 <html>
 <head>
     <script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
-    <script src="js/kickstart.js" type="text/javascript"></script>
-    <link href="css/kickstart.css" rel="stylesheet" type="text/css"/>
+
 </head>
 <body>
     
