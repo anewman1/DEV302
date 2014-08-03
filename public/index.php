ac20ebcb805c17f7f5e976c580ed5e7ed927f665
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-03 12:20:51 +1000

    Added Header and Footer includes based off a previous project

--- public/index.php
+++ public/index.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="DEV - The Fighting Mongooses">
+<?php
+    require 'includes/header.inc.php';
+?>
+
+    Content
     
-    <title>DEV_Store</title>
-    
-    <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
-    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-    
-</body>
-    
-    
-</html>
+<?php
+    require 'includes/footer.inc.php';
+?>
