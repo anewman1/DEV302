6446a4bcdb79814f282b8b6c4e8356ef5eadf20c
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-03 11:49:16 +1000

    Bootstrap and Jquery added.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
-
 <html>
 <head>
-    <script src="js/jquery-1.11.1.min.js" type="text/javascript"></script>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="DEV - The Fighting Mongooses">
+    
+    <title>DEV_Store</title>
+    
+    <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
     
