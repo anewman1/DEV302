a7cfd624e4e62b0d3da59c8bb84d83b34e87f0b4
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-14 16:25:27 +1000

    NewCommit

--- private/db.connect.php
+++ private/db.connect.php
@@ -1,8 +1,8 @@
 <?php
     $database = 'dev302_shop';
     $hostname = 'localhost';
-    $username = 'devadmin';
-    $password = 'admin';
+    $username = 'dev302';
+    $password = 'dev302';
 
     $pdo = new PDO("mysql:hostname=$hostname;dbname=$database", $username, $password);
 ?>
