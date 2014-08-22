95ce59481b33c30070db96c62d6472f3ee1db63f
AaronHumphreys <aaron_h23@hotmail.com>
2014-08-22 13:58:50 +1000

    Phili's Branch

---
+++ public/productDetail.php
@@ -0,0 +1,141 @@
+<?php
+    require 'includes/header.inc.php';
+?>
+
+<!-- OPTIONAL BEST SELLER/HIGHEST RATING PANEL -->
+<!-- Best Seller -->
+				<div class="col-lg-12 col-md-12 col-sm-12 visible-lg visible-md">
+					<div class="no-padding">
+	            		<span class="title">BEST SELLER</span>
+	            	</div>
+		                <div class="thumbnail col-lg-12 col-md-12 col-sm-6 text-center">
+		                	<a href="#" class="link-p">
+		                    	<img src="" alt="">
+		                	</a>
+		                    <div class="caption prod-caption">
+		                        <h4><a href="#"></a></h4>
+		                        <p></p>
+		                        <p>
+		                        	<div class="btn-group">
+			                        	<a href="#" class="btn btn-default">$ </a>
+			                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+		                        	</div>
+		                        </p>
+		                    </div>
+		                </div>
+		                <div class="thumbnail col-lg-12 col-md-12 col-sm-6 hidden-xs text-center">
+		                	<a href="detail.html" class="link-p">
+		                    	<img src="" alt="">
+		                	</a>
+		                    <div class="caption prod-caption">
+		                        <h4><a href="#"></a></h4>
+		                        <p></p>
+		                        <p>
+		                        	<div class="btn-group">$ </a>
+			                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+		                        	</div>
+		                        </p>
+		                    </div>
+		                </div>
+				</div>
+				<!-- End Best Seller -->
+
+        	</div>
+
+        	<div class="clearfix visible-sm"></div>
+
+			<!-- Product Detail -->
+        	<div class="col-lg-9 col-md-9 col-sm-12">
+        		<div class="col-lg-12 col-sm-12">
+            		<span class="title">The Hangover II</span>
+            	</div>
+	            <div class="col-lg-12 col-sm-12 hero-feature">
+            		<div class="sp-wrap">
+						<a href=""><img src="" alt=""></a>
+						<a href=""><img src="" alt=""></a>
+						<a href=""><img src="" alt=""></a>
+						<a href=""><img src="" alt=""></a>
+						<a href=""><img src="" alt=""></a>
+					</div>
+					<h4>The Hangover II</h4>
+					98 items in stock
+					<hr/>
+					The Hangover Part II is a 2011 American comedy film produced by Legendary Pictures and distributed by Warner Bros. Pictures. It is the sequel to 2009's The Hangover and the second film in The Hangover franchise.
+					<hr/>
+					<h3>$ 19.99</h3>
+					<div class="input-qty-detail">
+						<input type="text" class="form-control input-qty text-center" value="1">
+						<button class="btn btn-primary pull-left">add to cart</button>
+					</div>
+					<br/>
+					<hr/>
+					<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+						<a class="addthis_button_preferred_1"></a>
+						<a class="addthis_button_preferred_2"></a>
+						<a class="addthis_button_preferred_3"></a>
+						<a class="addthis_button_preferred_4"></a>
+						<a class="addthis_button_compact"></a>
+					</div>
+					<script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=xa-4f0c254f1302adf8"></script>
+	            </div>
+	            <div class="clearfix"></div>
+	            <div class="col-lg-12 col-sm-12">
+            		<span class="title">RELATED PRODUCTS</span>
+            	</div>
+            	<div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="detail.html" class="link-p">
+	                    	<img src="images/product-10.jpg" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"></a>Product Name</h4>
+	                        <p>Product Description</p>
+	                        <p>
+	                        	<div class="btn-group">
+		                        	<a href="#" class="btn btn-default">$ 22.99</a>
+		                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="detail.html" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="detail.html">Product Name</a></h4>
+	                        <p>Product Description</p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 15.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#">Product Name</a></h4>
+	                        <p>Product Description</p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 19.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+        	</div>
+        	<!-- End Product Detail -->
+
+<?php
+    require 'includes/footer.inc.php';
+?>
--- public/products.php
+++ public/products.php
@@ -1,69 +1,180 @@
 <?php
     require 'includes/header.inc.php';
-	
-	//import the database connection script
-	require("../private/db.connect.php");
-	
-	$sort = (isset($_GET['sort']) ? ($_GET['sort']) : "prod.id DESC");
-	
-	function currentSort($option) {
-		global $sort;
-		if ($option == $sort) {
-			echo("selected");
-		}
-	}
-	
-	$query = "
-		SELECT prod.id, prod.name, con.rating, prod.qty, img.image 
-		FROM product AS prod
-		LEFT JOIN image AS img
-		
-		ON prod.id = img.product_id
-		LEFT JOIN content AS con
-		ON prod.content_id = con.id
-		ORDER BY $sort;
-			";
-	$sth = $pdo->query($query);
 ?>
 
-    <div id="content" class="clearfix">
-    	<div id="sort" class="clearfix">
-        	<p>Sort by: </p>
-            <form name="sort" method="get" action="products.php" class="input-group">
-                <span class="input-group-btn">
-                    <button type="submit" class="btn btn-default">
-                        <span class="glyphicon glyphicon-refresh"></span>
-                    </button>
-                </span>
-                <select name="sort" class="form-control" >
-            	  <option value="prod.id DESC" <?php currentSort("prod.id DESC"); ?>>New Releases</option>
-                  <option value="con.id" <?php currentSort("con.id"); ?>>Rating</option>
-            	  <option value="prod.name" <?php currentSort("prod.name"); ?>>Name</option>
-            	  <option value="prod.price" <?php currentSort("prod.price"); ?>>Price</option>
-            	  <option value="rate.stars" <?php currentSort("rate.stars"); ?>>Top Rated</option>
-            	</select>
-			</form>
-        </div>
-    	<div class="products clearfix">
+<!-- Catalogue -->
+        	<div class="col-lg-9 col-md-9 col-sm-12">
+        		<div class="col-lg-12 col-sm-12">
+            		<span class="title">PRODUCTS CATALOGUE</span>
+            	</div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="detail.html" class="link-p">
+	                    	<img src="images/product-1.jpg" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#">Transformers 4: Age of Exstinction</a></h4>
+	                        <p>As humanity picks up the pieces, following the conclusion of "Transformers: Dark of the Moon," Autobots and Decepticons have all but vanished from the face of the planet.</p>
+	                        <p>
+	                        	<div class="btn-group">
+		                        	<a href="#" class="btn btn-default">$ 34.99</a>
+		                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="images/product-8.jpg" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="detail.html">The Hangover II</a></h4>
+	                        <p>The Hangover Part II is a 2011 American comedy film produced by Legendary Pictures and distributed by Warner Bros. Pictures. It's the sequel to 2009s The Hangover and the 2nd film in The Hangover franchise.</p>
+	                        <p>
+	                        	<div class="btn-group">
+		                        	<a href="#" class="btn btn-default">$ 15.99</a>
+		                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#">The Inbetweeners 2</a></h4>
+	                        <p>The Inbetweeners 2 is a 2014 British comedy film and sequel to The Inbetweeners Movie, which is based on the E4 sitcom The Inbetweeners. It was written and directed by series creators Damon Beesley and Iain Morris.</p>
+	                        <p>
+	                        	<div class="btn-group">
+		                        	<a href="#" class="btn btn-default">$ 19.99</a>
+		                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"></a></h4>
+	                        <p></p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 19.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="detail.html" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"></a></h4>
+	                        <p></p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 18.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"></a></h4>
+	                        <p></p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 19.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"> </a></h4>
+	                        <p></p>
+	                        <p>
+	                        	<div class="btn-group">$ 22.99</a>
+		                        	<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="#" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"></a></h4>
+	                        <p></p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 15.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="col-lg-4 col-sm-4 hero-feature text-center">
+	                <div class="thumbnail">
+	                	<a href="detail.html" class="link-p">
+	                    	<img src="" alt="">
+	                	</a>
+	                    <div class="caption prod-caption">
+	                        <h4><a href="#"></a></h4>
+	                        <p></p>
+	                        <p>
+	                        	<div class="btn-group">
+	                        		<a href="#" class="btn btn-default">$ 19.99</a>
+	                        		<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Buy</a>
+	                        	</div>
+	                        </p>
+	                    </div>
+	                </div>
+	            </div>
+				<div class="text-center">
+		        	<ul class="pagination catalogue-pagination">
+						<li class="disabled"><a>First</a></li>
+						<li class="disabled"><a>Prev</a></li>
+						<li class="active"><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">Next</a></li>
+						<li><a href="#">Last</a></li>
+					</ul>
+				</div>
+        	</div>
+        	<!-- End Catalogue -->
+
+
 <?php
-	while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-?>
-            <a href="product.php?product=<?= $row['id'] ?>">
-                <div class="product">
-                    <h3><?= $row['name'] ?></h3>
-                    <div class="img-holder">
-                    	<img src="<?= $row['image'] ?>"/>
-                    </div>
-                    <p>Rated: <?= $row['rating'] ?></p>
-                    <button type="button" class="btn btn-lg btn-success">Product Page</button>
-                </div>
-            </a>
-<?php
-	} 
-?>
-    	</div>
-    </div>
-    
-<?php
-	require 'includes/footer.inc.php';
+    require 'includes/footer.inc.php';
 ?>
